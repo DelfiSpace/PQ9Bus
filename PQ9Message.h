52d4ac2b29023d67ebd118671ca34ec28ad07466
Casper <broekhuizen.casper@gmail.com>
2020-04-24 14:08:42 +0200

    Fix in setters + setPayloadSize + rename in variables

--- PQ9Message.h
+++ PQ9Message.h
@@ -18,21 +18,27 @@ class PQ9Message : public DataMessage
 
  public:
       virtual void setMessageType(unsigned char resp){
-          this->getPayload()[1] = 0;
+          this->getBuffer()[1] = resp;
       };
       virtual unsigned char getMessageType(){
-          return this->getPayload()[1];
+          return this->getBuffer()[1];
       };
 
       virtual void setService(unsigned char address){
-          this->getPayload()[0] = 0;
+          this->getBuffer()[0] = address;
       };
       virtual unsigned char getService(){
-          return this->getPayload()[0];
+          return this->getBuffer()[0];
       };
 
       virtual unsigned char* getDataPayload(){
-          return &(this->getPayload()[2]);
+          return &(this->getBuffer()[2]);
+      };
+      virtual void setPayloadSize(unsigned int size){
+          this->setSize(2+size);
+      };
+      virtual unsigned int getPayloadSize(){
+          return this->getSize() + 2;
       };
 };
 
